e727fd33e2d90558a3562e4bcba6b728f9d82670
SDX <282919162@qq.com>
2018-07-10 17:15:20 +0800

    BaseLib commit

--- app/src/main/java/com/shendx/ark/MainActivity.kt
+++ app/src/main/java/com/shendx/ark/MainActivity.kt
@@ -2,15 +2,11 @@ package com.shendx.ark
 
 import android.os.Bundle
 import com.shendx.ark.base.lib.ui.BaseActivity
-import com.shendx.ark.example.module.ExampleModuleTest
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        try {
-            ExampleModuleTest.log()
-        } catch (e:Exception){}
     }
 }
